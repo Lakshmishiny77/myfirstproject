01c4486e6269d9861c5b19ce42d2654cb65e946e
Lakshmi B <82225535+Lakshmishiny77@users.noreply.github.com>
2021-04-18 21:08:09 +0530

    Add files via upload

---
+++ MA_LIstFunctions.py
@@ -0,0 +1,70 @@
+##var = ["dhoni"]
+##print(dir(var))
+
+##1.append - The append() method appends an element to the end of the list
+
+##fruits = ['apple', 'banana', 'cherry']
+##fruits.append("orange")
+##print(fruits)
+
+##2.clear - The clear() method removes all the elements from a list.
+
+##fruits = ['apple', 'banana', 'cherry', 'orange']
+##fruits.clear()
+##print(fruits)
+
+##3.copy - The copy() method returns a copy of the specified list.
+
+##fruits = ["apple", "banana", "cherry"]
+##x = fruits.copy()
+##print(x)
+
+##4.count - The count() method returns the number of elements with the specified value.
+
+##fruits = ["apple", "banana", "cherry"]
+##x = fruits.count("cherry")
+##print(x)
+
+##5.extend - The extend() method adds the specified list elements (or any iterable) to the end of the current list.
+
+##fruits = ['apple', 'banana', 'cherry']
+##cars = ['Ford', 'BMW', 'Volvo']
+##fruits.extend(cars)
+##print(fruits)
+
+##6.index - The index() method returns the position at the first occurrence of the specified value.
+
+##fruits = ['apple', 'banana', 'cherry']
+##x = fruits.index("cherry")
+##print(x)
+
+##7.insert - The insert() method inserts the specified value at the specified position.
+
+##fruits = ['apple', 'banana', 'cherry']
+##fruits.insert(1, "orange")
+##print(fruits)
+
+##8.pop - The pop() method removes the element at the specified position.
+
+##fruits = ['apple', 'banana', 'cherry']
+##fruits.pop(1)
+##print(fruits)
+
+##9.remove - The pop() method removes the specified index.
+
+##thislist = ["apple", "banana", "cherry"]
+##thislist.remove("banana")
+##print(thislist)
+
+##10.reverse - The reverse() method reverses the sorting order of the elements.
+
+##fruits = ['apple', 'banana', 'cherry']
+##fruits.reverse()
+##print(fruits)
+
+##11.sort - The sort() method sorts the list ascending by default.You can also make a function to decide the sorting criteria(s).
+
+cars = ['Ford', 'BMW', 'Volvo']
+cars.sort()
+print(cars)
+
